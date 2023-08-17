ddf039159a78503f90186c4542ac6f7fb8b71222
GeorgiDishkov-Enter21 <60046070+GeorgiDishkov@users.noreply.github.com>
2023-08-17 10:18:35 +0300

    Fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,9 @@
 import { Route, Routes } from "react-router-dom";
 import StartGame from "./pages/StartGame";
-import InGame from "./pages/InGame";
 import EndGame from "./pages/EndGame";
+import InGame from "./pages/inGame";
 
 function App() {
-  // const state = useSelector((state: RootState) => state)
-  // const dispatch:ThunkDispatch<RootState , undefined, AnyAction> = useDispatch();
-
-  // useEffect(() => {
-  //   (async() => {
-  //     await dispatch(asyncQuestions({count:5 , difficulty:"hard"}))
-  //   })()
-  // },[])
-  // console.log(state);
 
   return (
     <Routes>
